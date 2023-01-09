0800b80052ed855244aa1fb7d9147e69eb21aa02
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-01-09 21:53:10 +0330

    StreetFighter Added

---
+++ EVOChampions/Game/StreetFighter/StreetFighter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVOChampions.Game.MortalCombat
+{
+    internal class StreetFighter : Game
+    {
+        public StreetFighter(Player player1, Player player2) : base(player1, player2, new StreetFighterRoundCreator(), 4) { }
+    }
+}
---
+++ EVOChampions/Game/StreetFighter/StreetFighterRound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVOChampions.Game.MortalCombat
+{
+    internal class StreetFighterRound : Round
+    {
+        public StreetFighterRound(Player player1, Player player2) : base(player1, player2) { }
+
+        public override void Start()
+        {
+            Winner = player1;
+        }
+    }
+}
