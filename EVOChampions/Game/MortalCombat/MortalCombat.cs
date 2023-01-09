1e4adfa862910cc8f2bf8e55ff84513ed72927ac
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-01-09 21:47:26 +0330

    MortalCombat added

---
+++ EVOChampions/Game/MortalCombat/MortalCombat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVOChampions.Game.MortalCombat
+{
+    internal class MortalCombat : Game
+    {
+        public MortalCombat(Player player1, Player player2) : base(player1, player2, new MortalCombatRoundCreator(), 3) { }
+    }
+}
---
+++ EVOChampions/Game/MortalCombat/MortalCombatGameCreator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVOChampions.Game.MortalCombat
+{
+    internal class MortalCombatGameCreator : GameCreator
+    {
+        public override MortalCombat Cteate(Player player1, Player player2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
