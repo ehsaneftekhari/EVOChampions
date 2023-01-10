b0bb3178e59d70ad6bb0ac827f461b8b1dd1e57d
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-01-11 03:09:20 +0330

    Bracket Added

---
+++ EVOChampions/Bracket/Block.cs
@@ -0,0 +1,191 @@
+﻿using EVOChampions.Games;
+using EVOChampions.Managers.AccountManagements;
+using System.Threading.Tasks.Dataflow;
+
+namespace EVOChampions.Bracket
+{
+    internal class Block
+    {
+        private TournamentUser? startPlayer;
+        private Game? game;
+        private Creator GameCreator;
+        private int navigatedCount;
+
+        public Block(Creator GameCreator)
+        {
+            if (GameCreator is null)
+                throw new ArgumentNullException(nameof(GameCreator));
+        }
+
+        public Block(Creator GameCreator, Block upBlock, Block downBlock) : this(GameCreator)
+        {
+            try
+            {
+                SetUpBlock(upBlock);
+                SetDownBlock(downBlock);
+            }
+            catch { throw; }
+        }
+
+
+        public int RoundNumber
+        {
+            get
+            {
+                if (UpBlock is null)
+                    return 1;
+                else
+                    return UpBlock.RoundNumber + 1;
+            }
+        }
+
+        public Game? Game
+        {
+            get => game;
+            private set
+            {
+                if (value != null)
+                {
+                    startPlayer = null;
+                    game = value;
+                }
+            }
+        }
+
+        public Block? UpBlock { get; private set; }
+
+        public Block? DownBlock { get; private set; }
+
+        public Block? NextBlock { get; private set; }
+
+        public TournamentUser? Player
+        {
+            get
+            {
+                if (Game == null)
+                    return startPlayer;
+                else
+                {
+                    Player? tempWinner = Game.Winner;
+                    if (tempWinner != null && tempWinner.Parent != null)
+                        return (TournamentUser)tempWinner.Parent!;
+                    else
+                        return null;
+                }
+            }
+            set
+            {
+                if (value != null)
+                {
+                    game = null;
+                    startPlayer = value;
+                    CkearBlocks();
+                }
+            }
+        }
+
+
+        public Game? CreateGame()
+        {
+            if (UpBlock is null || DownBlock is null)
+                return null;
+
+            return GameCreator.CteateGame(UpBlock!.Player!, DownBlock!.Player!);
+        }
+
+        public void Navigate(TournamentUser tournamentUser)
+        {
+            if (RoundNumber == 1)
+            {
+                MakeStarterBlock(tournamentUser);
+            }
+            else
+            {
+                NavigateBack(tournamentUser);
+            }
+
+            navigatedCount++;
+        }
+
+        private void NavigateBack(TournamentUser tournamentUser)
+        {
+            int upNavigated = UpBlock.navigatedCount;
+            int downNavigated = DownBlock.navigatedCount;
+            switch ((upNavigated > downNavigated, upNavigated == downNavigated))
+            {
+                case (true, false):
+                    NavigateDownBlock(tournamentUser);
+                    break;
+                case (false, true):
+                    RandomNavigateBack(tournamentUser);
+                    break;
+                case (false, false):
+                    NavigateUpBlock(tournamentUser);
+                    break;
+            }
+        }
+
+        private void RandomNavigateBack(TournamentUser tournamentUser)
+        {
+            Random random = new Random();
+            int randNumber = random.Next(1, 3);
+            if (randNumber == 1)
+                NavigateDownBlock(tournamentUser);
+            else
+                NavigateUpBlock(tournamentUser);
+        }
+
+        private void NavigateDownBlock(TournamentUser tournamentUser)
+        {
+            if (DownBlock is null)
+                throw new Exception();
+
+            DownBlock.Navigate(tournamentUser);
+        }
+
+        private void NavigateUpBlock(TournamentUser tournamentUser)
+        {
+            if (UpBlock is null)
+                throw new Exception();
+
+            UpBlock.Navigate(tournamentUser);
+        }
+
+        private void MakeStarterBlock(TournamentUser tournamentUser)
+        {
+            if (startPlayer is null && UpBlock is null && DownBlock is null)
+            {
+                Player = tournamentUser;
+                navigatedCount = 1;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public void SetUpBlock(Block upBlock)
+        {
+            if (upBlock is null)
+                throw new ArgumentNullException($"Argument {nameof(upBlock)} can not be null");
+
+            UpBlock = upBlock;
+            upBlock.NextBlock = this;
+        }
+
+        public void SetDownBlock(Block downBlock)
+        {
+            if (downBlock is null)
+                throw new ArgumentNullException($"Argument {nameof(downBlock)} can not be null");
+
+            DownBlock = downBlock;
+            downBlock.NextBlock = this;
+        }
+
+        private void CkearBlocks()
+        {
+            UpBlock = null;
+            DownBlock = null;
+        }
+    }
+}
---
+++ EVOChampions/Bracket/Bracket.cs
@@ -0,0 +1,122 @@
+﻿using EVOChampions.Games;
+using EVOChampions.Managers.AccountManagements;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks.Dataflow;
+
+namespace EVOChampions.Bracket
+{
+    public class Bracket
+    {
+        Block[]? blocks;
+        Block? fianl;
+
+        public Bracket(TournamentUser[] tournamentUsers, Creator gameCreator)
+        {
+            if (tournamentUsers is null)
+                throw new ArgumentNullException(nameof(tournamentUsers));
+
+            if (gameCreator is null)
+                throw new ArgumentNullException(nameof(gameCreator));
+
+            InitialBlocks(tournamentUsers.Length, out blocks!, out fianl, gameCreator);
+            InitialUsers(tournamentUsers, fianl!);
+        }
+
+        private void InitialBlocks (int UsersCount, out Block[] blocks, out Block? fianl, Creator gameCreator)
+        {
+            if (UsersCount == 0)
+                throw new ArgumentOutOfRangeException(nameof(UsersCount));
+
+            BracketCreator creator = new BracketCreator(UsersCount, out blocks, gameCreator);
+            creator.Create(out fianl);
+        }
+
+        private void InitialUsers(TournamentUser[] tournamentUsers, Block fianl)
+        {
+            if (tournamentUsers is null)
+                throw new ArgumentNullException(nameof(tournamentUsers));
+
+            if (fianl is null)
+                throw new ArgumentNullException(nameof(fianl));
+
+            for(int i = 0; i < tournamentUsers.Length; i++)
+            {
+                fianl.Navigate(tournamentUsers[i]);
+            }
+        }
+    }
+
+    internal class BracketCreator
+    {
+        private Creator gameCreator;
+        private Block[] blocks;
+        int lastIndex;
+        int NumberOFReounds;
+
+        public BracketCreator(int UsersCount, out Block[] blocks, Creator gameCreator)
+        {
+            var numbers = CalculateNubers(UsersCount);
+            NumberOFReounds = numbers.NumberOFReounds;
+            blocks = new Block[numbers.NuberOfBlocks];
+
+            this.blocks = blocks;
+
+            lastIndex = -1;
+
+            this.gameCreator = gameCreator;
+        }
+
+        public void Create(out Block? block)
+        {
+            CreateBackward(out block, NumberOFReounds);
+        }
+
+        private void CreateBackward(out Block block, int roundNumber)
+        {
+            block = null;
+            if (roundNumber > 0)
+            {
+                block = new Block(gameCreator);
+                blocks[++lastIndex] = block;
+
+                Block? newUpBlock = null;
+                CreateBackward(out newUpBlock, roundNumber - 1);
+
+                Block? newDownBlock = null;
+                CreateBackward(out newDownBlock, roundNumber - 1);
+
+                if (newUpBlock != null && newDownBlock != null)
+                {
+                    block.SetUpBlock(newUpBlock);
+                    block.SetDownBlock(newDownBlock);
+                }
+            }
+        }
+
+        private int GetNextPowerOf2(int number)
+        {
+            for (int i = 1; ; i++)
+            {
+                int powerOf2 = (int)Math.Pow(2, i);
+                if (powerOf2 >= number)
+                    return powerOf2;
+            }
+        }
+
+        private (int NumberOFReounds, int NuberOfBlocks) CalculateNubers(int Count)
+        {
+            (int NumberOFReounds, int NuberOfBlocks) result;
+
+            Count = GetNextPowerOf2(Count);
+            result.NuberOfBlocks = Count;
+            result.NumberOFReounds = 1;
+            while (Count > 1)
+            {
+                Count /= 2;
+                result.NuberOfBlocks += Count;
+                result.NumberOFReounds++;
+            }
+            return result;
+        }
+    }
+}
