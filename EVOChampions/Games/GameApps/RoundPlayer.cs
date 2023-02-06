a762a8089fc3ad49bfdef60f77f67fe1014b7cb7
Ehsan Eftekhari <ehsaneftekhari@outlook.com>
2023-02-07 00:19:15 +0330

    DecreaseHealth and UndoDamage added to RoundPlayer

--- EVOChampions/Games/GameApps/RoundPlayer.cs
+++ EVOChampions/Games/GameApps/RoundPlayer.cs
@@ -1,12 +1,18 @@
 ﻿using EVOChampions.Managers;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace EVOChampions.Games.GameApps
 {
     public sealed class RoundPlayer : ParentChildrenKeeper
     {
+        private int lastDamage;
         public string UserName => ((GameAppPlayer)Parent!).UserName;
 
-        public RoundPlayer(GameAppPlayer player) : base(player) => Health = 100;
+        public RoundPlayer(GameAppPlayer player) : base(player) 
+        {
+            Health = 100;
+            lastDamage = 0;
+        }
 
         public int Health { get; private set; }
 
@@ -14,5 +20,15 @@ namespace EVOChampions.Games.GameApps
         {
             return string.Format("{0} Health: {1}", UserName, Health);
         }
+
+        public void DecreaseHealth(int damage)
+        {
+            lastDamage = damage;
+            Health -= damage;
+            if (Health < 0)
+                Health = 0;
+        }
+
+        public void UndoDamage() => Health += lastDamage;
     }
 }
